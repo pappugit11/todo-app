850b7d85f51b24c97e632cae1c3a17d63a5021e3
pappugit11 <pappuram619@gmail.com>
2024-08-19 22:14:22 +0530

    third commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,60 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Todo = () => {
-    const [inputData, setInputData] = useState('');
-    const [items, setItems] = useState([]);
-    const addItem = (e) =>{
-        e.preventDefault();
-        if(!inputData){
-            
-        }
-        else{
-            setItems([...items, inputData]);
-            setInputData('')
-        }
+  const [inputData, setInputData] = useState("");
+  const [items, setItems] = useState([]);
+  const addItem = (e) => {
+    e.preventDefault();
+    if (!inputData) {
+    } else {
+      setItems([...items, inputData]);
+      setInputData("");
     }
+  };
 
-    const deleteItem = (id) =>{
-        console.log(id);
+  const deleteItem = (id) => {
+    console.log(id);
 
-        const updatedItems = items.filter((elm, ind)=>{
-            return ind !== id;
-            
-        });
-        setItems(updatedItems)
-    }
-    const removeAll = (id) =>{
-        setItems([])
-    }
+    const updatedItems = items.filter((elm, ind) => {
+      return ind !== id;
+    });
+    setItems(updatedItems);
+  };
+  const removeAll = (id) => {
+    setItems([]);
+  };
   return (
     <>
-    <div className="app_wrapper">
+      <div className="app_wrapper">
         <div className="form_wrap">
-            <form>
-                <input type="text" placeholder='Enter Name' value={inputData} onChange={(e)=> setInputData(e.target.value)} />
-                <button onClick={addItem}>+</button>
-            </form>
+          <form>
+            <input
+              type="text"
+              placeholder="Enter Name Here"
+              value={inputData}
+              onChange={(e) => setInputData(e.target.value)}
+            />
+            <button onClick={addItem}>+</button>
+          </form>
         </div>
         <div className="todo_list">
-        {
-            items.map((elm, ind)=>{
-                return(
-                    <div className="todo_list_item" key={ind}>
-                        <span>{elm}</span>
-                        <button onClick={()=> deleteItem(ind)}>delete</button>
-                    </div>
-                )
-            })
-        }
+          {items.map((elm, ind) => {
+            return (
+              <div className="todo_list_item" key={ind}>
+                <span>{elm}</span>
+                <button onClick={() => deleteItem(ind)}>delete</button>
+              </div>
+            );
+          })}
         </div>
 
-        <div className='remove_wrap'>
-            <button onClick={removeAll}>Remove All</button>
+        <div className="remove_wrap">
+          <button onClick={removeAll}>Remove All</button>
         </div>
-
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;

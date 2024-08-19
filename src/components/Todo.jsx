433445fcd5d1e21fe10dd26b34cc4422dc218108
pappugit11 <pappuram619@gmail.com>
2024-08-19 21:53:34 +0530

    first commit

---
+++ src/components/Todo.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+const Todo = () => {
+    const [inputData, setInputData] = useState('');
+    const [items, setItems] = useState([]);
+    const addItem = (e) =>{
+        e.preventDefault();
+        if(!inputData){
+            
+        }
+        else{
+            setItems([...items, inputData]);
+            setInputData('')
+        }
+    }
+
+    const deleteItem = (id) =>{
+        console.log(id);
+
+        const updatedItems = items.filter((elm, ind)=>{
+            return ind !== id;
+            
+        });
+        setItems(updatedItems)
+    }
+    const removeAll = (id) =>{
+        setItems([])
+    }
+  return (
+    <>
+    <div className="app_wrapper">
+        <div className="form_wrap">
+            <form>
+                <input type="text" placeholder='Enter Name' value={inputData} onChange={(e)=> setInputData(e.target.value)} />
+                <button onClick={addItem}>+</button>
+            </form>
+        </div>
+        <div className="todo_list">
+        {
+            items.map((elm, ind)=>{
+                return(
+                    <div className="todo_list_item" key={ind}>
+                        <span>{elm}</span>
+                        <button onClick={()=> deleteItem(ind)}>delete</button>
+                    </div>
+                )
+            })
+        }
+        </div>
+
+        <div className='remove_wrap'>
+            <button onClick={removeAll}>Remove All</button>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Todo
